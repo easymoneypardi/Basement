7c22eb1f7e47593bdd19356b63e5a3c534139a75
easymoneypardi <mathias.pardi@gmail.com>
2020-03-18 00:51:34 +0100

    thrid commit

--- js/script.js
+++ js/script.js
@@ -45,20 +45,40 @@ function getDrinks() {
 }
 
 function getGames() {
-    fetch('https://api.myjson.com/bins/17c8vu')
+    fetch('https://api.myjson.com/bins/nycqe')
         .then((res) => res.json())
         .then((data) => {
             let output = '<h2 class="display-4 mb-4">Brætspil</h2>';
-            data.forEach(function (drinks) {
+            data.forEach(function (games) {
                 output += `
               <ul class="list-group mb-3">
-                <li class="list-group-item"><strong>Produkt:</strong> ${drinks.product}</li>
-                <li class="list-group-item"><img class="img-fluid img" width="50%" src=${drinks.image} alt="basement"></li>
-                <li class="list-group-item"><strong>Pris:</strong> ${drinks.prices}</li>
-                <li class="list-group-item"><strong>2 for:</strong> ${drinks.for}</li>
+                <li class="list-group-item"><strong>Brætspil:</strong> ${games.product}</li>
+                <li class="list-group-item"><img class="img-fluid img" width="50%" src=${games.image} alt="basement"></li>
+                <li class="list-group-item"><strong>Antal spillere:</strong> ${games.litre}</li>
+                <li class="list-group-item"><strong>Tid:</strong> ${games.prices}</li>
               </ul>
             `;
             })
             document.getElementById('output').innerHTML = output;
         })
+}
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = -2; // For Safari
+  document.documentElement.scrollTop = -2; // For Chrome, Firefox, IE and Opera
 }
